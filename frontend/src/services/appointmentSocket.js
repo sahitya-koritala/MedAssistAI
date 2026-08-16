eb4c207adb2ec4f51269930de29d54e59fed027e
sahitya-koritala <sahityakoritala@gmail.com>
2026-08-16 17:53:02 +0530

    Change socket connection URL to production

--- frontend/src/services/appointmentSocket.js
+++ frontend/src/services/appointmentSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000', {
+const socket = io('https://medassistai-5.onrender.com', {
   autoConnect: false,
   transports: ['websocket'],
 });
